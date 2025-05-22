fa3b68ef5fae3641c8ce5eafa86a9331b447c226
SaidakbarX <khayumkhujayev6937@gmail.com>
2025-05-22 12:06:48 +0500

    Add project files.

---
+++ UserContacts.Server/Middlewares/NightBlockMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace UserContacts.Server.Middlewares;
+
+public class NightBlockMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public NightBlockMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var currentHour = DateTime.Now.Hour;
+
+        if (currentHour >= 18 || currentHour < 9 || context.Request.Path.ToString().Contains("get"))
+        {
+            context.Response.StatusCode = 403;
+            await context.Response.WriteAsJsonAsync(new
+            {
+                message = "Our APIs are open from 9:00 to 18:00"
+            });
+
+            return; 
+        }
+
+        await _next(context); 
+    }
+}
